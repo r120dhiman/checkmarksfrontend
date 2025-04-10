207c71285c9dc388d49cc0c23af0c4719fa0d0f2
Rohit Dhiman <r120dhiman@gmail.com>
2025-04-10 22:15:31 +0530

    Staging

--- src/Pages/AfterPayment.jsx
+++ src/Pages/AfterPayment.jsx
@@ -13,7 +13,7 @@ useEffect(() => {
     const auth="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlJvaGl0IEt1bWFyIiwiZW1haWwiOiJyb2hpdC5rdW1hckBnbWFpbC5jb20iLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNzQ0MjI5NzU4LCJleHAiOjE3NDQzMTYxNTh9.Uh9nzzqsUbFEb_oB5uhGrYDWT3wty-_hkpnr6D-W7OU"
     try {
       if (paymentId) {
-        const response = await axios.post('http://localhost:3000/payment/newpayment', 
+        const response = await axios.post('https://checkmarksbackend.onrender.com/payment/newpayment', 
           { paymentId ,auth}
         );
         console.log(response.data);
